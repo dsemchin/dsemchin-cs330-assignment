72d3a2b9b10f4066afc87d6a2863afcbf8a0ef6e
dsemchin <118320568+dsemchin@users.noreply.github.com>
2022-11-18 11:28:50 -0600

    Update daniel semchin keypad engine.py
    fsm is working until lock and unlock states

--- daniel semchin keypad engine.py
+++ daniel semchin keypad engine.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Nov 17 09:15:47 2022
 
-@author: semch
+@author: daniel semchin
 """
 
 import unittest
@@ -10,59 +10,64 @@ import re
 import collections
 
 class state:
-    def __init__(self, name, args, output):
+    def __init__(self, name, output):
         self.name = name
-        self.args = None
+        self.args = {}
         self.output = output
         self.current = False
         
+    def append_args(self, arg, next_state):
+        self.args.update({arg : next_state})
+        
     def parse(self,char):
-        try:
-            print(self.args(char))
-            return self.args(char)
-            
+        print(char)
+        try: 
+            #print("transition")
+            return self.args.get(char)
         except:
             return self
         
     def __repr__(self):
-        return self.output
+        return str((self.name))#, self.args))
         
 class fsm:
     def __init__(self):
-        self.states = set({})
+        self.states = []
         self.input = None
         self.current = None
+        
+    def __repr__(self):
+        return str(self.states)
     
     def append(self, state):
-        #unittest.assertIsInstance()
-        self.states.add(state)
+        self.states.append(state)
         
     def generate_states(self, passcode):
         # append lock and unlock states
-        unlock_state = None
-        lock_state = None
-        unlock_state = state(passcode+'1',{'1':unlock_state,'4':lock_state},'UNLOCKED')
-        lock_state = state(passcode+'4',{'4':lock_state,'1':unlock_state},'LOCKED')
-        self.append(unlock_state)
-        self.append(lock_state)
+        unlock_state = state(passcode+'1','UNLOCKED')
+        lock_state = state(passcode+'4','LOCKED')
         
-        # append completed passcode state
-        complete_state = state(passcode,{'1':unlock_state,'4':lock_state},'NONE')
+        complete_state = state(passcode,'NONE')
+        complete_state.args.update({'1':unlock_state,'4':lock_state})
+        print(complete_state.args)
         self.append(complete_state)
         
         # append partial passcode states
         next_state = complete_state
-        for i in range(len(passcode)-2,0):
-            partial_state = state(passcode[0:i],{passcode[i]:next_state},'NONE')
+        for i in range(len(passcode)-1,0,-1):
+            partial_state = state(passcode[0:i],'NONE')
+            partial_state.args.update({passcode[i]:next_state})
             self.append(partial_state)
             next_state = partial_state
 
         # empty state
-        empty_state = state('empty',{passcode[0]:next_state},'NONE')
+        empty_state = state('empty','NONE')
+        empty_state.args.update({passcode[0]:next_state})
         self.append(empty_state)
         
         # setting start state for FSM
         self.current = empty_state
+        print(self)
         
     def find(self, name):
         return
@@ -82,9 +87,16 @@ class fsm:
             #print(entry)
             while entry != collections.deque([]):
                 key = entry.popleft()
-                self.current = self.current.parse(key)
-                print(self.current.name)
-
+                new_state = self.current.parse(key)
+                if self.current == new_state:
+                    self.current = self.current
+                    print('NONE')
+                if type(new_state) == type(None):
+                    self.current = self.states[-1]
+                else:
+                    self.current = new_state
+                    print(self.current.output)
+                print(self.current)
 
 passcode = input("Create a passcode (leave blank for '2255'): ")
 if passcode == '':
@@ -92,4 +104,3 @@ if passcode == '':
     print(passcode)
 keypad = fsm()
 keypad.run(passcode)
-          
